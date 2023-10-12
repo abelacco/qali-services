33679d214f2335e345d955605e9d414b12bd402b
cbenz <contactocbenitez@gmail.com>
2023-10-12 14:41:35 -0300

    add type constraint to doctor model props

--- src/doctor/entities/doctor.entity.ts
+++ src/doctor/entities/doctor.entity.ts
@@ -2,31 +2,33 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema()
-export class Doctor extends Document{
+export class Doctor extends Document {
+  @Prop({
+    // unique: true,
+    // index: true,
+    type: String,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    // index: true,
+    type: String,
+  })
+  phone: string;
 
-    @Prop({
-        // unique: true,
-        // index: true,
-    })
-    name: string;
-    @Prop({
-        unique: true,
-        // index: true,
-    })
-    phone: string;
-    
-    @Prop({
-        // unique: true,
-        // index: true,
-    })
-    speciality: string;
+  @Prop({
+    // unique: true,
+    // index: true,
+    type: String,
+  })
+  speciality: string;
 
-    @Prop({
-        // unique: true,
-        // index: true,
-    })
-    fee: number;
-
+  @Prop({
+    // unique: true,
+    // index: true,
+    type: Number,
+  })
+  fee: number;
 }
 
 export const DoctorSchema = SchemaFactory.createForClass(Doctor);
